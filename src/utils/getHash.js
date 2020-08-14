397de9c92357c4790918ba7edd438c02c50e287d
D4v1d_D4art3 <jesusdavidduartejimenez@gmail.com>
2020-08-14 15:20:14 -0400

    implementacion de un trigger

--- src/utils/getHash.js
+++ src/utils/getHash.js
@@ -8,6 +8,7 @@ const getHash = () =>
 
 export default getHash;
 
+
 // location.hash trae el fragmento de la url a partir de donde encuentre un #. En este caso traería #/1/
 // .slice(1) corta la url y muestra a partir del primer elemento. En este Caso el resultado es /1/
 // .toLocaleLowerCase() convierte a minúscula la cadena
